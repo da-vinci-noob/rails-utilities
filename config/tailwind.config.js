31966cb80f27331d1a66f49aca0021a96beb3a0f
Shah Rukh <shah.xhtml@yahoo.com>
2024-10-11 12:47:14 +0530

    feat: Add Shadcn Vue radix-vue

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,23 +1,40 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const animate = require('tailwindcss-animate')
 
+/** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: ['class'],
   content: [
-    './public/*.html',
-    './app/helpers/**/*.rb',
-    './app/javascript/**/*.js',
-    './app/frontend/**/*.vue',
-    './app/views/**/*.{erb,haml,html,slim}'
+    './app/frontend/components/**/*.{ts,tsx,vue}',
+    './app/frontend/pages/**/*.{ts,tsx,vue}',
+    './app/frontend/lib/**/*.vue',
+    './app/views/**/*.{erb,haml,html,slim}',
+    './public/*.html'
   ],
+  prefix: '',
   theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px'
+      }
+    },
     extend: {
-      fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans]
+      keyframes: {
+        'accordion-down': {
+          from: { height: 0 },
+          to: { height: 'var(--radix-accordion-content-height)' }
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: 0 }
+        }
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out'
       }
     }
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/container-queries')
-  ]
+  plugins: [animate]
 }
