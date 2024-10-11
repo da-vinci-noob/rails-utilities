1bfd37c51520290566fe741040ef039b6fdd94bd
Shah Rukh <shah.xhtml@yahoo.com>
2024-10-11 10:44:38 +0530

    fix: errors

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,7 +41,7 @@ Rails.application.configure do
   config.active_support.disallowed_deprecation_warnings = []
 
   # Highlight code that enqueued background job in logs.
-  config.active_job.verbose_enqueue_logs = true
+  # config.active_job.verbose_enqueue_logs = true
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
