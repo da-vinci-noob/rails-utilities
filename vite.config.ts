88d697898c2bd035fe155d58b59cb7478d99e25d
Shah Rukh <shah.xhtml@yahoo.com>
2024-10-11 11:44:03 +0530

    Config: vite; ts; path

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
+import path from 'node:path'
 import { defineConfig } from 'vite'
 import RubyPlugin from 'vite-plugin-ruby'
 import vue from '@vitejs/plugin-vue'
 import FullReload from 'vite-plugin-full-reload'
+
+const aliases = {
+  '/@/': path.resolve(__dirname, './app/frontend') // Replace 'src' with your actual source directory
+}
 
 export default defineConfig({
   plugins: [
@@ -9,4 +14,7 @@ export default defineConfig({
     FullReload(['config/routes.rb', 'app/views/**/*']),
     vue()
   ],
+  resolve: {
+    alias: aliases
+  }
 })
