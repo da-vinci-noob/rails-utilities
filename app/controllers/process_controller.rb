fdd126f43a54834492f5e814b85a575a0b5384ae
Shah Rukh <shah.xhtml@yahoo.com>
2024-10-13 02:09:01 +0530

    fix: Rubocop

--- app/controllers/process_controller.rb
+++ app/controllers/process_controller.rb
@@ -1,41 +1,61 @@
 # frozen_string_literal: true
 
+# Contains all the processes done for an utility
 class ProcessController < ApplicationController
   def benchmark
-    message1 = params[:message1]
-    message2 = params[:message2]
+    message1, message2 = benchmark_params.values_at(:message1, :message2)
 
     begin
       raise ArgumentError, 'Code Blocks cannot be empty' if message1.blank? || message2.blank?
 
-      time = Benchmark.bm do |x|
-        x.report('message1:') { SafeRuby.eval(message1) }
-        x.report('message2:') { SafeRuby.eval(message2) }
-      end
-
-      # Respond with a success message
-      render json: {
-        code1:   {
-          cpu_time:  format('%.10f', time.first.utime),
-          real_time: format('%.10f', time.first.real)
-        },
-        code2:   {
-          cpu_time:  format('%.10f', time.last.utime),
-          real_time: format('%.10f', time.last.real)
-        },
-        success: true
-      }, status: :ok
+      results = perform_benchmark(message1, message2)
+      render_success(results)
     rescue ArgumentError => e
-      render json: { success: false, message: e.message }
+      render_blank_message(e.message)
     rescue StandardError => e
-      puts e.inspect
-      render json: { success: false, message: "Check your Code Block, #{remove_not_needed_output(e.message)}" }
+      log_and_render_error(e)
     end
   end
 
   private
 
+  def benchmark_params
+    params.require(:process).permit(:message1, :message2)
+  end
+
   def remove_not_needed_output(error_string)
     error_string.gsub(%r{/[^\s]+:\d+|eval}, '')
   end
+
+  def perform_benchmark(message1, message2)
+    Benchmark.bm do |x|
+      x.report('message1:') { SafeRuby.eval(message1) }
+      x.report('message2:') { SafeRuby.eval(message2) }
+    end
+  end
+
+  def render_success(results)
+    render json: {
+      code1:   {
+        cpu_time:  format('%.10f', results.first.utime),
+        real_time: format('%.10f', results.first.real)
+      },
+      code2:   {
+        cpu_time:  format('%.10f', results.last.utime),
+        real_time: format('%.10f', results.last.real)
+      },
+      success: true
+    }, status: :ok
+  end
+
+  def render_blank_message(message)
+    render json: { success: false, message: }
+  end
+
+  # rubocop:disable Naming/MethodParameterName
+  def log_and_render_error(e)
+    Rails.logger.error e.inspect
+    render json: { success: false, message: "Check your Code Block, #{remove_not_needed_output(e.message)}" }
+  end
+  # rubocop:enable Naming/MethodParameterName
 end
